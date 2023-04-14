1b911c75ff9661b783428b8965e43f2c3718f2d4
David Mohrhardt <9117625+DavidMohrhardt@users.noreply.github.com>
2023-04-14 14:51:59 -0700

    Move plugin to experimental

--- Src/Modules/Experimental/Plugins/Loader/WindowsPluginLoader.cpp
+++ Src/Modules/Experimental/Plugins/Loader/WindowsPluginLoader.cpp
@@ -2,6 +2,9 @@
 
 #if SAVANNA_WINDOWS
 
+// TODO: For some reason the compiler just will not load the Kernel32 library. I have no idea why.
+#pragma lib("kernel32.dll")
+
 // #define WINDOWS_LEAN_AND_MEAN 1
 #include <libloaderapi.h>
 
