09fdbe9cb5f81c00bc21dac5a4144b014236c88f
David Mohrhardt <9117625+DavidMohrhardt@users.noreply.github.com>
2023-09-15 11:18:41 -0700

    Make floating point unions work a little nicer in C++ land

--- Src/Runtime/Types/Primitive/FloatingPointUnions.h
+++ Src/Runtime/Types/Primitive/FloatingPointUnions.h
@@ -25,6 +25,7 @@ typedef union se_float16
         se_uint16 m_Mantissa : 10;
     };
 } se_float16;
+typedef se_float16 se_half16;
 
 /**
  * @brief TODO @DavidMohrhardt Document
@@ -49,11 +50,6 @@ typedef union se_float32
         se_uint32 m_Mantissa : 23;
     };
 } se_float32;
-
-/**
- * @brief A namespaced type definition for the type se_float32.
- */
-DECLARE_SAVANNA_NAMESPACED_CPP_TYPE_DEF(se_float32, float32);
 
 /**
  * @brief TODO @DavidMohrhardt Document
@@ -83,5 +79,103 @@ typedef union se_float64
 } se_float64;
 typedef se_float64 se_double64;
 
-DECLARE_SAVANNA_NAMESPACED_CPP_TYPE_DEF(se_float64, float64);
-DECLARE_SAVANNA_NAMESPACED_CPP_TYPE_DEF(se_double64, double64);
+#ifdef __cplusplus
+
+namespace Savanna
+{
+    struct float16
+    {
+        se_float16 m_Value;
+
+        float16() = default;
+        float16(const float16&) = default;
+        float16(float16&&) = default;
+        float16& operator=(const float16&) = default;
+        float16& operator=(float16&&) = default;
+
+        constexpr float16(float value)
+            : m_Value{ .m_Sign = 0, .m_Exponent = 0, .m_Mantissa = 0 }
+        {
+            se_float32 temp = { .m_SinglePrecision = value };
+            m_Value.m_Sign = temp.m_Sign;
+            m_Value.m_Exponent = temp.m_Exponent;
+            m_Value.m_Mantissa = temp.m_Mantissa;
+        }
+
+        constexpr operator float()
+        {
+            se_float32 temp = { .m_Sign = m_Value.m_Sign, .m_Exponent = m_Value.m_Exponent, .m_Mantissa = m_Value.m_Mantissa };
+            return temp.m_SinglePrecision;
+        }
+
+        constexpr operator se_float16&()
+        {
+            return m_Value;
+        }
+    };
+
+    struct float32
+    {
+        se_float32 m_Value;
+
+        float32() = default;
+        float32(const float32&) = default;
+        float32(float32&&) = default;
+        float32& operator=(const float32&) = default;
+        float32& operator=(float32&&) = default;
+
+        constexpr float32(float value)
+            : m_Value{ .m_SinglePrecision = value }
+        {}
+
+        constexpr operator float()
+        {
+            return m_Value.m_SinglePrecision;
+        }
+
+        constexpr operator float&()
+        {
+            return m_Value.m_SinglePrecision;
+        }
+
+        constexpr operator se_float32&()
+        {
+            return m_Value;
+        }
+    };
+
+    struct float64
+    {
+        se_float64 m_Value;
+
+        float64() = default;
+        float64(const float64&) = default;
+        float64(float64&&) = default;
+        float64& operator=(const float64&) = default;
+        float64& operator=(float64&&) = default;
+
+        constexpr float64(double value)
+            : m_Value{ .m_DoublePrecision = value }
+        {}
+
+        constexpr operator double()
+        {
+            return m_Value.m_DoublePrecision;
+        }
+
+        constexpr operator double&()
+        {
+            return m_Value.m_DoublePrecision;
+        }
+
+        constexpr operator se_float64&()
+        {
+            return m_Value;
+        }
+    };
+
+    using half = float16;
+    using double64 = float64;
+}
+
+#endif // __cplusplus
